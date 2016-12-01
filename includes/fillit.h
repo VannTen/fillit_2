1e89854a3c2a3b6e4a1281ad7ef6b2b392e6cef8
Max Gautier <mgautier@student.42.fr>
2016-12-01 11:43:35 +0100

    Shortens struct part
    Shortens indexs to x y

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 17:53:56 by mgautier          #+#    #+#             */
-/*   Updated: 2016/11/30 18:30:50 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/01 11:43:11 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 
 typedef struct			s_piece_part
 {
-	int x_index;
-	int y_index;
+	int x;
+	int y;
 }						t_piece_part;
 typedef struct			s_piece_origin
 {
-	size_t x_index;
-	size_t y_index;
+	size_t x;
+	size_t y;
 }						t_piece_origin;
 typedef t_piece_origin	t_decalage;
 typedef struct			s_tetriminos
