d715d35d58638b98f506bac7aaf66ea1b6890235
Max Gautier <mgautier@student.42.fr>
2016-12-01 18:03:41 +0100

    add header file for ft_relative_position

---
+++ includes/ft_relative_position.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_relative_position.h                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/01 17:39:44 by mgautier          #+#    #+#             */
+/*   Updated: 2016/12/01 17:41:24 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_RELATIVE_POSITION_H
+# define FT_RELATIVE_POSITION_H
+
+# include "fillit.h"
+
+t_rel_position			**ft_relative_positions(char **piece_description);
+
+#endif
