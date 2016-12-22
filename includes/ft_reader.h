bd9f0e7bd644522e6968f52f580861168e93b9ab
Max Gautier <mgautier@student.42.fr>
2016-12-22 19:06:08 +0100

    add proto to header

--- includes/ft_reader.h
+++ includes/ft_reader.h
@@ -6,7 +6,7 @@
 /*   By: chbechet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 13:58:33 by chbechet          #+#    #+#             */
-/*   Updated: 2016/12/22 14:22:13 by chbechet         ###   ########.fr       */
+/*   Updated: 2016/12/22 18:34:40 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 
 char	*ft_piece_reader(const int fd);
 t_fifo	*ft_file_reader(const int fd);
+t_bool	ft_good_place_back_n(const char *piece);
+t_bool	ft_valid_count(const char *piece);
 
 
 #endif
