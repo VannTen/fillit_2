43b4505f40bde0c70a18b774d44db7a4ddf47d32
Max Gautier <mgautier@student.42.fr>
2016-12-23 11:55:16 +0100

    Move validation functions into one file
    Gather ft_valid_count and ft_good_place_back_n in
    ft_valid_pieces_format.c
    + create ft_valid_pieces_format which validates a link containing the
    char string

--- srcs/ft_valid_pieces_format.c
+++ srcs/ft_valid_pieces_format.c
@@ -1,18 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_valid_count.c                                   :+:      :+:    :+:   */
+/*   ft_valid_pieces_format.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chbechet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/20 17:54:51 by chbechet          #+#    #+#             */
-/*   Updated: 2016/12/22 18:55:54 by mgautier         ###   ########.fr       */
+/*   Created: 2016/12/23 11:42:02 by mgautier          #+#    #+#             */
+/*   Updated: 2016/12/23 11:47:58 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_bool		ft_valid_count(const char *piece)
+static t_bool		ft_good_place_back_n(const char *piece)
+{
+	unsigned int	index;
+	unsigned int	count;
+
+	index = 0;
+	count = 0;
+	while (piece[index])
+	{
+		while (piece[index] != '\n')
+		{
+			index++;
+			count++;
+		}
+		if (piece[index] == '\n' && count != 4)
+			return (FALSE);
+		else
+		{
+			count = 0;
+			index++;
+		}
+	}
+	return (TRUE);
+}
+
+static t_bool		ft_valid_count(const char *piece)
 {
 	unsigned int		point;
 	unsigned int		dieze;
@@ -37,4 +62,12 @@ t_bool		ft_valid_count(const char *piece)
 		return (TRUE);
 	else
 		return (FALSE);
+}
+
+t_bool				ft_valid_pieces_format(const t_lst *link)
+{
+	char *piece;
+
+	piece = (char*)link->content;
+	return (ft_valid_count(piece) && ft_good_place_back_n(piece));
 }
