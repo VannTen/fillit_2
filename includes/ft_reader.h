c06eef5004125ca8bc01f25ba75c8e56a83e0285
Max Gautier <mgautier@student.42.fr>
2016-12-23 12:14:10 +0100

    Norme

--- includes/ft_reader.h
+++ includes/ft_reader.h
@@ -6,7 +6,7 @@
 /*   By: chbechet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 13:58:33 by chbechet          #+#    #+#             */
-/*   Updated: 2016/12/23 11:48:07 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/23 12:13:57 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 char	*ft_piece_reader(const int fd);
 t_fifo	*ft_file_reader(const int fd);
 t_bool	ft_valid_pieces_format(const t_lst *link);
-
 
 #endif
