85081c13757386ee6aa0d11ab81a8acd0689f19e
chbechet <chbechet@student.42.fr>
2017-01-07 17:33:59 +0100

    Creation Struct s_resolution

---
+++ includes/solver.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/06 16:19:52 by chbechet          #+#    #+#             */
+/*   Updated: 2017/01/07 17:16:58 by chbechet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLVER_H
+# define SOLVER_H
+
+typedef struct		s_resolution
+{
+	unsigned int	size_tab;
+	char			**tab;
+}					t_resolution;
+
+#endif
