013171ae99a1e71fdb3a757788498cb9b578f65b
chbechet <chbechet@student.42.fr>
2017-01-08 15:50:46 +0100

    Modif empty tab and Norme

--- srcs/ft_empty_tab.c
+++ srcs/ft_empty_tab.c
@@ -6,9 +6,11 @@
 /*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/28 14:33:14 by chbechet          #+#    #+#             */
-/*   Updated: 2017/01/05 19:19:12 by chbechet         ###   ########.fr       */
+/*   Updated: 2017/01/08 15:49:30 by chbechet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "fillit.h"
 
 void	ft_empty_tab(char **tab, int x, int y, t_tetrimino *piece)
 {
@@ -18,10 +20,10 @@ void	ft_empty_tab(char **tab, int x, int y, t_tetrimino *piece)
 	index = 0;
 	tab[x][y] = '\0';
 	coord = piece->relative_coordinates;
-	while (i < 3)
+	while (index < 3)
 	{
 		tab[x + coord->x][y + coord->y] = '\0';
 		coord++;
-		i++;
+		index++;
 	}
 }
