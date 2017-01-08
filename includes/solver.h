34913946ea1cb31a1344d88b79e129cfa2e3036f
chbechet <chbechet@student.42.fr>
2017-01-08 15:54:36 +0100

    Change prototype to empty_tab

--- includes/solver.h
+++ includes/solver.h
@@ -6,7 +6,7 @@
 /*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/06 16:19:52 by chbechet          #+#    #+#             */
-/*   Updated: 2017/01/08 12:04:07 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/08 15:57:22 by chbechet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,5 @@ static t_bool		ft_try_every_position(t_resolution *array,
 											const t_lst *tetri);
 void				ft_add_tetri(t_resolution *array,
 									int x, int y, t_tetrimino *piece);
-void				ft_empty_tab(t_resolution *array, t_tetrimino *piece,
-									int x, int y);
+void				ft_empty_tab(char **tab, int x, int y, t_tetrimino *piece);
 #endif
