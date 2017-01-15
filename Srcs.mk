9410d8146dd7276fcff5c3f6784619d1f1ea7aa2
Max Gautier <mgautier@student.42.fr>
2017-01-15 12:28:54 +0100

    Rename file

--- Srcs.mk
+++ Srcs.mk
@@ -6,7 +6,7 @@
 #    By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/12/19 07:58:53 by mgautier          #+#    #+#              #
-#*   Updated: 2017/01/14 18:35:42 by mgautier         ###   ########.fr       *#
+#*   Updated: 2017/01/15 12:35:18 by mgautier         ###   ########.fr       *#
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,11 +14,11 @@
 # (Even if empty, DO NOT REMOVE any variable definition)
 
 TARGET := fillit
-SRC := ft_add_tetri.c ft_generate_resolve_array.c ft_display_string_array.c \
+SRC := ft_add_remove_tetri.c ft_generate_resolve_array.c \
 	ft_file_reader.c ft_is_placable.c ft_parser.c ft_piece_reader.c \
 	ft_relative_position.c ft_solver.c ft_string_to_coordinates.c \
-	ft_transform_tetri.c ft_valid_pieces_format.c main.c
-
+	ft_transform_tetri.c ft_valid_pieces_format.c ft_display_string_array.c \
+	main.c
 # Directories
 
 SRC_DIR :=
